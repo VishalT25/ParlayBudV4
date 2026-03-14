4ffc4964212749637989990536f6880899fa3e7f
VishalT25 <vthamaraimanalan@gmail.com>
2026-03-14 10:51:40 -0400

    chore: gitignore .vercel/ build output

--- parlaybudv4/.vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/entries/pages/model/_page.ts.js
+++
@@ -1,8 +0,0 @@
-const load = async ({ fetch }) => {
-  const res = await fetch("/metrics.json");
-  const metrics = await res.json();
-  return { metrics };
-};
-export {
-  load
-};
--- parlaybudv4/.vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.WWTs7PW1.js","_app/immutable/chunks/CrlIH5jG.js","_app/immutable/chunks/mxgC9ynA.js","_app/immutable/chunks/hk8jYNMC.js","_app/immutable/chunks/BawvSKyM.js","_app/immutable/chunks/B5Ts1cDx.js","_app/immutable/chunks/KAczVnXl.js","_app/immutable/chunks/rgW7MAQh.js"];
-export const stylesheets = ["_app/immutable/assets/0.BGevV9kC.css"];
-export const fonts = [];
